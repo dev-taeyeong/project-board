f961f3b492421f9bd04538404e71730dacac8e91
dev.taeyeong <dev.taeyeong@gmail.com>
2022-07-10 14:26:17 +0900

    #12 - AuditingFields 추출
    생성자, 생성일지, 수정자, 수정일시는 반복적으로 엔티티 클래스에 들어가는 요소이고, 도메인과 직접 연관 없는 요소이므로 추출이 가능하다.
    
    `@MappedSuperclass`를 이용하여 상속 방식으로 추출
    
    그외 `@DateTimeFormat`요소 추가 및 jpa 옵션 개선

--- project-board/src/main/java/com/taeyeong/projectboard/domain/Article.java
+++ project-board/src/main/java/com/taeyeong/projectboard/domain/Article.java
@@ -1,14 +1,8 @@
 package com.taeyeong.projectboard.domain;
 
 import lombok.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -22,11 +16,11 @@ import java.util.Set;
         @Index(columnList = "createdBy")
 })
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Setter
@@ -44,21 +38,6 @@ public class Article {
     @OrderBy("id")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; // 생성일시
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; // 생성자
-
-    @LastModifiedDate
-    private LocalDateTime modifiedAt; // 수정일시
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; // 수정자
 
     private Article(String title, String content, String hashtag) {
         this.title = title;
